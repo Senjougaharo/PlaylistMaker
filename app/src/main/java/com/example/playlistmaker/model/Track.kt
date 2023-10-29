4f6ee920d05d5a7e704ccc40debd81ad49d34348
Senjougaharo <shashliknauzhin@yandex.ru>
2023-10-29 16:40:10 +0300

    13 спринт конец

--- app/src/main/java/com/example/playlistmaker/model/Track.kt
+++ app/src/main/java/com/example/playlistmaker/model/Track.kt
@@ -1,12 +1,26 @@
 package com.example.playlistmaker.model
 
 import android.widget.TextView
+import java.io.Serializable
+import java.text.SimpleDateFormat
+import java.util.Locale
 
 data class Track(
     val trackName: String,
     val artistName: String,
     val trackTimeMillis: Long,
-    val artworkUrl100: String
-)
+    val artworkUrl100: String,
+    val primaryGenreName: String,
+    val releaseDate: String,
+    val collectionName : String,
+    val country : String
+) : Serializable
+
+{
+    fun getCoverArtwork() = artworkUrl100.replaceAfterLast('/',"512x512bb.jpg")
+
+    fun timeFormat() = SimpleDateFormat("mm:ss", Locale.getDefault()).format(trackTimeMillis)
+}
+
 
 
