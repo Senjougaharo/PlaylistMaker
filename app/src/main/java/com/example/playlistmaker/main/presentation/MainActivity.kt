034a72bd031c22988c5a08e9a81d6a4128765b47
Senjougaharo <shashliknauzhin@yandex.ru>
2024-01-15 17:06:13 +0300

    18 спринт начало

--- app/src/main/java/com/example/playlistmaker/main/presentation/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/main/presentation/MainActivity.kt
@@ -1,38 +1,38 @@
 package com.example.playlistmaker.main.presentation
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import com.example.playlistmaker.R
+import androidx.appcompat.app.AppCompatActivity
+import com.example.playlistmaker.databinding.ActivityMainBinding
 import com.example.playlistmaker.media.presentation.MediaActivity
 import com.example.playlistmaker.search.presentation.SearchActivity
 import com.example.playlistmaker.settings.presentation.SettingsActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val searchButton = findViewById<Button>(R.id.search_button)
 
-        searchButton.setOnClickListener {
+        binding.searchButton.setOnClickListener {
 
             val searchIntent = Intent(this, SearchActivity::class.java)
             startActivity(searchIntent)
         }
 
 
-        val mediaButton = findViewById<Button>(R.id.media_button)
 
-        mediaButton.setOnClickListener {
+        binding.mediaButton.setOnClickListener {
 
             val mediaIntent = Intent(this, MediaActivity::class.java)
             startActivity(mediaIntent)
         }
-        val settingsButton = findViewById<Button>(R.id.settings_button)
 
-        settingsButton.setOnClickListener{
+
+        binding.settingsButton.setOnClickListener{
 
             val settingsIntent = Intent(this, SettingsActivity::class.java)
 
