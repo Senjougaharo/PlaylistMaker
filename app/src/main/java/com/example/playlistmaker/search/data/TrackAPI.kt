da83cc997579686ef6ecb4b3e1309a1de97c9cb8
Senjougaharo <shashliknauzhin@yandex.ru>
2024-03-03 13:59:23 +0300

    20 спринт корутины начало

--- app/src/main/java/com/example/playlistmaker/search/data/TrackAPI.kt
+++ app/src/main/java/com/example/playlistmaker/search/data/TrackAPI.kt
@@ -1,11 +1,11 @@
 package com.example.playlistmaker.search.data
 
-import retrofit2.Call
+import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface TrackAPI {
 
     @GET("/search?entity=song")
-        fun getTrack(@Query("term")term:String) : Call<TrackSearchResponse>
+    suspend fun getTrack(@Query("term") term: String): Response<TrackSearchResponse>
 }
